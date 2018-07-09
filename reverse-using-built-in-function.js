6e2e832b1e5fb65bf06866176b90e4bf66daaeca
Sankalp Johri <sankalp.johri@iiitb.org>
2018-07-09 11:25:48 +0530

    Added reverse functions built-in and iterator

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,7 +1,5 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = (s) => {
-    return s;
-};
+const reverse = s => s.split('').reverse().join('');
 
 module.exports = reverse;
--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,4 +1,11 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
-const reverse = () => {};
+
+const reverse = (s) => {
+  const reverseArray = [];
+  for (let i = s.length - 1; i >= 0; i -= 1) {
+    reverseArray.push(s.charAt(i));
+  }
+  return reverseArray.join('');
+};
 
 module.exports = reverse;
